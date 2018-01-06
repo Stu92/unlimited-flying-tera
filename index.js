15a1436bc2ff7564805032f3da2253e1278acf67
Ztu <32149990+Stu92@users.noreply.github.com>
2018-01-06 02:00:37 -0600

    Ignores flight stamina restrictions.

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = function FlyMore(dispatch) {
+	let outOfEnergy = false
+
+	dispatch.hook('S_CANT_FLY_ANYMORE', 1, () => false)
+
+	dispatch.hook('S_PLAYER_CHANGE_FLIGHT_ENERGY', 1, event => { outOfEnergy = event.energy === 0 })
+
+	dispatch.hook('C_PLAYER_FLYING_LOCATION', 3, event => {
+		if(outOfEnergy && event.type !== 7 && event.type !== 8 && event.type !==5) {
+			event.type = 5
+
+					return true
+		}
+	})
+}
